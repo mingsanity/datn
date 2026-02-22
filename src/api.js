903c7af3e1cc9f2fa3cee218a9d7206c562c3bd6
mingsanity <mingsanityyyy@gmail.com>
2026-02-23 04:10:06 +0700

    thay doi

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-export const API_BASE = "http://localhost:8080";
+export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export function getToken() {
   return localStorage.getItem("token");
